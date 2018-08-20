bbb645c30d9e8f3410ad2cf4734797a5c2623afc
nersonSwift <42254802+nersonSwift@users.noreply.github.com>
2018-08-20 23:51:11 +0300

    Initial Commit

---
+++ firtWork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firtWork
+//
+//  Created by Александр Сенин on 20.08.2018.
+//  Copyright © 2018 Александр Сенин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
